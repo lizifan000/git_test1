2e1cefcb4a53882091475770352049c847e903fc
lizifan <lizifan@forver.com.cn>
2022-06-24 21:43:21 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("HelloGit!");
         System.out.println("HelloGit3!");
         System.out.println("HelloGit4!");
+        System.out.println("hot-fix-test!");
     }
 }
